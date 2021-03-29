75b0b0e3db608a69d4d05b74e77abe94883e88cd
easrng <easrng@gmail.com>
2021-03-29 16:55:00 -0400

    Update status.js

--- status.js
+++ status.js
@@ -1,9 +1,7 @@
 (function(){
   // Used throughout the whole script
-  let isDomReady, slo, sli, domReady;
-  domReady=new Promise(cb=>{
-    document.addEventListener("DOMContentLoaded", cb)
-  })
+  const API_ORIGIN = "https://scratchstatus-api.glitch.me"
+  let isDomReady, slo, sli, statusMenu, domReady, cip;
   // Used for events.
   window.scratchStatus={};
   window.scratchStatus.onUserChange=function onUserChange(){
@@ -15,6 +13,7 @@
         slo.style.display="inline"
         sli.style.display="none"
       }
+    if(scratchNotifier.status.tokens[scratchNotifier.mainUsername]) startPing()
   }
   
   // Initialize scratchNotifier.status
@@ -38,18 +37,47 @@
     }
   }
   
-  function login(){
-    location.href="https://scratchstatus-api.glitch.me/v1/auth/" + encodeURIComponent(scratchNotifier.mainUsername) + "/" + encodeURIComponent(location.origin+location.pathname+"?statusLogin=1");
+  function startPing() {
+    if (cip) clearInterval(cip);
+    async function ping() {
+      if (token && ping) {
+        let r = await fetch(API_ORIGIN+"/v1/checkin",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ token: scratchNotifier.status.tokens[scratchNotifier.mainUsername] })
+        });
+        if (r.status == 403) {
+          logout()
+          M.Modal.getInstance(document.querySelector("#loggedout")).open()
+        }
+      }
+    }
+    cip = setInterval(ping, 1000 * 60 * 5);
+    ping();
   }
   
-  document.addEventListener("DOMContentLoaded", function(){
-    isDomReady=true;
-    // Set up elements
-    // Only show the new feature indicator for first-time users from now until May 1st 2021
-    document.querySelector("#status-new").style.display=(Date.now()<1619841600000 && !scratchNotifier.status.hasLoggedInEver)?"inline":"none";
-    slo = document.querySelector("#status-logged-out")
-    sli = document.querySelector("#status-logged-in")
-    document.querySelector("#status-login").addEventListener("click", login);
-    window.scratchStatus.onUserChange()
+  function login(){
+    location.href=API_ORIGIN+"/v1/auth/" + encodeURIComponent(scratchNotifier.mainUsername) + "/" + encodeURIComponent(location.origin+location.pathname+"?statusLogin=1");
+  }
+  
+  domReady=new Promise(cb=>{
+    document.addEventListener("DOMContentLoaded", function(){
+      isDomReady=true;
+      // Set up elements
+      // Only show the new feature indicator for first-time users from now until May 1st 2021
+      document.querySelector("#status-new").style.display=(Date.now()<1619841600000 && !scratchNotifier.status.hasLoggedInEver)?"inline":"none";
+      slo = document.querySelector("#status-logged-out")
+      sli = document.querySelector("#status-logged-in")
+      statusMenu = document.querySelector("#status-menu")
+      statusMenu.selectedIndex=scratchNotifier.status.invisible?1:0;
+      statusMenu.addEventListener("change", function (){
+        scratchNotifier.status.invisible=statusMenu.value=="0";
+        updateLocalStorage()
+      })
+      document.querySelector("#status-login").addEventListener("click", login);
+      cb()
+      window.scratchStatus.onUserChange()
+    })
   })
 })();
