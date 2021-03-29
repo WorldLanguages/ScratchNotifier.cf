cdbf2073297af62b1e5a15f2b335f7e9a423029c
easrng <easrng@gmail.com>
2021-03-29 14:58:23 -0400

    Update status.js

--- status.js
+++ status.js
@@ -1,4 +1,6 @@
 (function(){
+  // Used throughout the whole script
+  let domReady, slo, sli
   // Used for events.
   window.scratchStatus={};
   // Initialize scratchNotifier.status
@@ -8,20 +10,22 @@
   scratchNotifier.status.invisible=scratchNotifier.status.invisible||false;
   updateLocalStorage()
   window.scratchStatus.onUserChange=function onUserChange(){
-    if(scratchNotifier.status.tokens[scratchNotifier.mainUsername]){
-      slo.style.display="inline"
-      sli.style.display="none"
-    } else {
-      sli.style.display="inline"
-      slo.style.display="none"
-    }
+    if(domReady)
+      if(scratchNotifier.status.tokens[scratchNotifier.mainUsername]){
+        slo.style.display="inline"
+        sli.style.display="none"
+      } else {
+        sli.style.display="inline"
+        slo.style.display="none"
+      }
   }
   document.addEventListener("DOMContentLoaded", function(){
+    domReady=true;
     // Set up elements
     // Only show the new feature indicator for first-time users from now until May 1st 2021
     document.querySelector("#status-new").style.display=(Date.now()<1619841600000 && !scratchNotifier.status.hasLoggedInEver)?"inline":"none";
-    let slo = document.querySelector("#status-logged-out")
-    let sli = document.querySelector("#status-logged-in")
+    slo = document.querySelector("#status-logged-out")
+    sli = document.querySelector("#status-logged-in")
     window.scratchStatus.onUserChange()
   })
 })();
