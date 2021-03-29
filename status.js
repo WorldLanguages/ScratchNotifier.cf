54acb1e35cd58636abdc6388d2c18d8314ba39b8
easrng <easrng@gmail.com>
2021-03-29 15:38:42 -0400

    Update status.js

--- status.js
+++ status.js
@@ -3,12 +3,6 @@
   let domReady, slo, sli
   // Used for events.
   window.scratchStatus={};
-  // Initialize scratchNotifier.status
-  scratchNotifier.status=scratchNotifier.status||{}
-  scratchNotifier.status.tokens=scratchNotifier.status.tokens||{};
-  scratchNotifier.status.hasLoggedInEver=scratchNotifier.status.hasLoggedInEver||false;
-  scratchNotifier.status.invisible=scratchNotifier.status.invisible||false;
-  updateLocalStorage()
   window.scratchStatus.onUserChange=function onUserChange(){
     if(domReady)
       if(scratchNotifier.status.tokens[scratchNotifier.mainUsername]){
@@ -19,9 +13,30 @@
         sli.style.display="none"
       }
   }
+  
+  // Initialize scratchNotifier.status
+  scratchNotifier.status=scratchNotifier.status||{}
+  scratchNotifier.status.tokens=scratchNotifier.status.tokens||{};
+  scratchNotifier.status.hasLoggedInEver=scratchNotifier.status.hasLoggedInEver||false;
+  scratchNotifier.status.invisible=scratchNotifier.status.invisible||false;
+  updateLocalStorage()
+  
+  // Check if we just logged in
+  let usp=new URLSearchParams(location.search)
+  if(usp.get("statusLogin")){
+    history.pushState("","",location.origin+location.pathname) // strip hash and query
+    if(usp.get("status")=="success"&&usp.get("token")){
+      scratchNotifier.status.tokens[scratchNotifier.mainUsername]=usp.get("token")
+      window.scratchStatus.onUserChange()
+    } else {
+      swal({text:"Looks like you denied permission to update your status. Please log in again."})
+    }
+  }
+  
   function login(){
     location.href="https://scratchstatus-api.glitch.me/v1/auth/" + encodeURIComponent(scratchNotifier.mainUsername) + "/" + encodeURIComponent(location.origin+location.pathname+"?statusLogin=1");
   }
+  
   document.addEventListener("DOMContentLoaded", function(){
     domReady=true;
     // Set up elements
